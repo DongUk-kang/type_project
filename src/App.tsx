fb8d2280a690547eb2d72113b1c0e80a3d1fee1f
dong uk <gusrn1255@naver.com>
2021-09-18 20:09:42 +0900

    add Navbar #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import tw from "twin.macro"
+import "./App.css"
+import { MainPage } from "./app/containers/HomePage"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `};
+`;
+
+const App = () => {
+    return (
+        <AppContainer>
+          <MainPage />
+        </AppContainer>
+    );
+};
 
 export default App;
---
+++ src/app/components/logo/index.tsx
@@ -0,0 +1,60 @@
+// import React from 'react';
+// import styled from "styled-components";
+// import tw from "twin.macro"
+// import CarLogoImg from "../../../assets/logo.png"
+// import CarLogoDarkImg from "../../../assets/logo_dark.png"
+//
+// interface ILogoProps {
+//     color?: "white" | "dark",
+//     bgColor?: "white" | "dark"
+// }
+//
+// const LogoContainer = styled.div`
+//   ${tw`
+//     flex
+//     items-center
+//   `};
+// `;
+//
+// const LogoText = styled.div`
+//   ${tw`
+//     text-xl
+//     md:text-2xl
+//     font-bold
+//     text-black
+//     m-1
+//     `};
+//   ${({ color }: any) => (color === "white" ? tw`text-white` : tw`text-black`)}
+// ` as any;
+//
+// const Image = styled.div`
+//   width: auto;
+//   ${tw`h-6 md:h-9`};
+//
+//   img {
+//     width: auto;
+//     height: 100%;
+//   }
+// `;
+//
+// function Logo (props: ILogoProps) {
+//
+//     const {color, bgColor} = props
+//
+//     return (
+//         <LogoContainer>
+//             <Image>
+//                 <img src={bgColor === "dark" ? CarLogoDarkImg : CarLogoImg } />
+//             </Image>
+//             <LogoText color={ color || "dark"}>
+//                 YourCar.
+//             </LogoText>
+//         </LogoContainer>
+//     );
+// };
+//
+// export default Logo;
+
+import Logo from "./Logo";
+
+export { Logo }
